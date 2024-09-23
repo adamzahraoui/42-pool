2bb507379d4486420c855b7218e9d1ee507fdf2c
RidHell <145365475+azahraou@users.noreply.github.com>
2024-09-23 12:45:37 +0100

    pool 1337

--- C02/ex10/ft_strlcpy.c
+++ C02/ex10/ft_strlcpy.c
@@ -3,23 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: adzahrao <adzahrao@student.42.fr>          +#+  +:+       +#+        */
+/*   By: adam <adam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/31 09:34:16 by adzahrao          #+#    #+#             */
-/*   Updated: 2024/09/01 03:25:06 by adzahrao         ###   ########.fr       */
+/*   Updated: 2024/09/23 12:17:37 by adam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-
-unsigned int	ft_strlcpy(char	*dest, char	*src, unsigned int size)
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
-	unsigned int	i;
-	unsigned int	n;
+	unsigned int i;
+	unsigned int src_len;
 
-	n = 0;
-	while (src[n] != '\0')
-		n++;
+	src_len = 0;
+	while (src[src_len] != '\0')
+		src_len++;
 	if (size > 0)
 	{
 		i = 0;
@@ -30,5 +28,5 @@ unsigned int	ft_strlcpy(char	*dest, char	*src, unsigned int size)
 		}
 		dest[i] = '\0';
 	}
-	return (n);
+	return (src_len);
 }
--- C02/ex11/ft_putstr_non_printable.c
+++ C02/ex11/ft_putstr_non_printable.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putstr_non_printable.c                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: adzahrao <adzahrao@student.42.fr>          +#+  +:+       +#+        */
+/*   By: adam <adam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/31 14:51:37 by adzahrao          #+#    #+#             */
-/*   Updated: 2024/09/01 04:02:01 by adzahrao         ###   ########.fr       */
+/*   Updated: 2024/09/23 12:14:19 by adam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,25 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void	ft_putstr_non_printable(char	*str)
+void	ft_putstr_non_printable(char *str)
 {
-	char	*had;
+	char	*hex;
 	int		i;
 
-	had = "0123456789abcdef";
+	hex = "0123456789abcdef";
 	i = 0;
 	while (str[i] != '\0')
 	{
-		if (str[i] >= 0 && str[i] <= 31)
+		if (str[i] < 32 || str[i] > 126)
 		{
 			ft_putchar('\\');
-			ft_putchar(had[(str[i] / 16)]);
-			ft_putchar(had[(str[i] % 16)]);
+			ft_putchar(hex[(unsigned char)str[i] / 16]);
+			ft_putchar(hex[(unsigned char)str[i] % 16]);
 		}
 		else
-			write(1, &str[i], 1);
+		{
+			ft_putchar(str[i]);
+		}
 		i++;
 	}
 }
